79f2d9e55cd56e5ea1e1c0d5759ffb7c69be056d
Alvin2800 <139653846+Alvin2800@users.noreply.github.com>
2026-03-18 21:13:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ humidity = 0
 
 @app.route("/data")
 def data():
-    global temperature, humidity
+    try:
+        temp = float(request.args.get("temp", 0))
+        hum = float(request.args.get("hum", 0))
 
-    temperature = float(request.args.get("temp", 0))
-    humidity = float(request.args.get("hum", 0))
+        print("Temp:", temp)
+        print("Hum:", hum)
 
-    print("Température :", temperature)
-    print("Humidité :", humidity)
+        return "OK", 200
 
-    return "OK", 200
-
+    except Exception as e:
+        print("Erreur:", e)
+        return "Erreur", 500
 
 @app.route("/temperature")
 def get_temperature():
