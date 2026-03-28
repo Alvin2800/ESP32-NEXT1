adf7d35cf983e48c61f33a86d84c6f69005dedf1
Alvin2800 <139653846+Alvin2800@users.noreply.github.com>
2026-03-28 20:47:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 temperature = 0
 humidity = 0
 
+@app.route("/")
+def home():
+    return "ESP32 IoT Server Running"
+
 @app.route("/data")
 def data():
     global temperature, humidity
@@ -12,8 +16,8 @@ def data():
     temperature = request.args.get("temp", 0)
     humidity = request.args.get("hum", 0)
 
-    print("Température :", temperature)
-    print("Humidité :", humidity)
+    print("Température :", temperature, flush=True)
+    print("Humidité :", humidity, flush=True)
 
     return "OK", 200
 
@@ -21,11 +25,6 @@ def data():
 def get_temperature():
     return jsonify({"temperature": temperature})
 
-
 @app.route("/humidity")
 def get_humidity():
     return jsonify({"humidity": humidity})
-
-
-             
-             
